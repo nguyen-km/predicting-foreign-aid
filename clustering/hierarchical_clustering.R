412d069012caf1a2f7ea9a358e10956eb953940b
Kevin Nguyen <kevin.m.nguyen@colorado.edu>
2023-02-25 22:16:07 -0700

    Update hierarchical_clustering.R

--- clustering/hierarchical_clustering.R
+++ clustering/hierarchical_clustering.R
@@ -8,10 +8,13 @@ df_num = dplyr::select_if(df, is.numeric)# only select numeric data
 df_num_scaled = scale(df_num) # normalize the data
 rownames(df_num_scaled) = df$ISO3 # Keep country names
 
-
+# Use cosine similarity as distance metric
 X = as.dist(philentropy::distance(df_num_scaled, method="cosine", use.row.names = TRUE))
 
 hc = hclust(X, method = 'ward.D2')
+
+#plot dendrogram
 plot(hc)
 abline(h = 0.95, col = 'red')
+
 y_pred = cutree(hc, h = 0.95) # predictions
